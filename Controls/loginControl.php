df0a7c9f058f6051e82958096deaa9d79af45fd3
cartelesantana <mandeimelda13@gmail.com>
2023-07-13 09:19:07 +0100

    login correction

--- Controls/loginControl.php
+++ Controls/loginControl.php
@@ -1,52 +1,54 @@
 <?php
 
-        $servername = "localhost";
-        $DbUser = "root";
-        $DbPwd = "";
-        $dbname = "oicData";
-        $username=$_POST['Username'];
-        $pass=$_POST['password'];
-        //connect to database using Mysqli method
-        try {
-            $conn = new mysqli($servername, $DbUser, $DbPwd, $dbname);
 
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }else{
-                session_start();
+           $servername = "localhost";
+           $DbUser = "root";
+           $DbPwd = "";
+           $dbname = "oicData";
+           $username=$_POST['Username'];
+           $pass=$_POST['password'];
+           //connect to database using Mysqli method
+           try {
+               $conn = new mysqli($servername, $DbUser, $DbPwd, $dbname);
 
-                $query="select * from users where username=  '$username'";
-                $user=$conn->query($query);
-                if($user){
-                    if(mysqli_num_rows($user) > 0){
-                        $result = $user -> fetch_assoc();
-                        if(password_verify($pass,$result['pwd'])){
-                            $UserId=$result['id'];
-                            header("location:../index.php?id=$UserId");
-                        }else{
+               if ($conn->connect_error) {
+                   die("Connection failed: " . $conn->connect_error);
+               }else{
+                   session_start();
 
-                            echo "
+                   $query="select * from users where username=  '$username'";
+                   $user=$conn->query($query);
+                   if($user){
+                       if(mysqli_num_rows($user) > 0){
+                           $result = $user -> fetch_assoc();
+                           if(password_verify($pass,$result['pwd'])){
+                               $UserId=$result['id'];
+                               header("location:../index.php?id=$UserId");
+                           }else{
+
+                               echo "
                                 <script> 
                                     alert('password is wrong');
                                     window.location.replace('../Forms/login.php');
                                 </script>
                             ";
 
-                        }
-                    }else echo "
+                           }
+                       }else echo "
                                 <script> 
                                     alert('User does not exist');
                                     window.location.replace('../Forms/login.php');
                                 </script>
                     ";
-                }else{
-                    echo "we had a problem";
-                }
-            }
-            $conn->close();
-        }catch (Exception $e){
-            echo $e->getTrace();
-        }
+                   }else{
+                       echo "we had a problem";
+                   }
+               }
+               $conn->close();
+           }catch (Exception $e){
+               echo $e->getTrace();
+           }
+
 
 
 
